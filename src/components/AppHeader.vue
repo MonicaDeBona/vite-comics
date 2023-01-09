1cf63a5ce6e6c0f980ba9e56137357df1f352d7c
MonicaDeBona <debona.monica@outlook.it>
2023-01-09 17:34:23 +0100

    Implement for loop on main navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,14 +5,68 @@
                 <img src="../assets/imgs/dc-logo.png" alt="DC main logo">
             </div>
             <ul>
-                <li></li>
+                <li v-for="navItem in navItems" :class=" (navItem.active) ? 'active' : '' ">
+                    {{ navItem.text }}
+                </li>
             </ul>
         </nav>
     </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            navItems: [
+                {
+                    text : "CHARACTERS",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "COMICS",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "MOVIES",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "TV",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "GAMES",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "VIDEOS",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "FANS",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "NEWS",
+                    url: "#",
+                    active: false,
+                },
+                {
+                    text : "SHOP",
+                    url: "#",
+                    active: false,
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <style lang="scss">
@@ -28,10 +82,21 @@
             width: 100%;
             display: flex;
             justify-content: space-between;
+            align-items: center;
         }
 
         .logo img {
             width: 80px;
+        }
+    }
+
+    nav ul {
+        display: flex;
+        justify-content: space-between;
+        list-style-type: none;
+        
+        li {
+            padding-right: 1.5rem;
         }
     }
 </style>
